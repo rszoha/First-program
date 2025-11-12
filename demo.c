8a83a8989371de432047610103c9e585abe0d548
rszoha <rszoha195@gmail.com>
2025-11-12 19:05:03 +0530

    Add files via upload
    finding the marks of subject

---
+++ demo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+//int marks1=97;
+//int marks2=98;
+//int marks3=89;
+
+int marks[3];
+printf("enter phy :");
+scanf("%d",&marks[0]);
+
+printf("enter chem :");
+scanf("%d",&marks[1]);
+
+printf("enter math : ");
+scanf("enter",&marks[2]);
+
+printf("phy = %d, chem =%d, math =%d",marks[0], marks[0],marks[0]);
+
+return 0;
+
+
+}
