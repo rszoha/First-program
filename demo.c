d6816d206c9457119aba4d600278afcf26b24a52
rszoha <rszoha195@gmail.com>
2025-11-12 19:11:32 +0530

    Add files via upload

--- demo.c
+++ demo.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 
 int main(){
-//int marks1=97;
-//int marks2=98;
-//int marks3=89;
-
-int marks[3];
-printf("enter phy :");
-scanf("%d",&marks[0]);
-
-printf("enter chem :");
-scanf("%d",&marks[1]);
-
-printf("enter math : ");
-scanf("enter",&marks[2]);
-
-printf("phy = %d, chem =%d, math =%d",marks[0], marks[0],marks[0]);
+    print("hello world");
 
 return 0;
 
